c319127c5dcd4d51a89b70bde4d1ebc395da2963
jpogran <james.pogran@puppet.com>
2018-04-27 11:25:05 -0400

    fix profile path

--- setup.ps1
+++ setup.ps1
@@ -1,39 +1,39 @@
 $config = @(
   @{
     Source = "~/.gitconfig"
-    Link   = "~/src/my/profile/git/gitconfig"
+    Link   = "~/src/profile/git/gitconfig"
   },
   @{
     Source = "~/.gitignore"
-    Link   = "~/src/my/profile/git/gitignore"
+    Link   = "~/src/profile/git/gitignore"
   },
   @{
     Source = "~/Documents/PowerShell/profile.ps1"
-    Link   = "~/src/my/profile/pwsh/profile.ps1"
+    Link   = "~/src/profile/pwsh/profile.ps1"
   },
   @{
     Source = "~/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1"
-    Link   = "~/src/my/profile/pwsh/profile.ps1"
+    Link   = "~/src/profile/pwsh/profile.ps1"
   },
   @{
     Source = "~/.gemrc"
-    Link   = "~/src/my/profile/ruby/gemrc"
+    Link   = "~/src/profile/ruby/gemrc"
   },
   @{
     Source = "~/.pryrc"
-    Link   = "~/src/my/profile/ruby/pryrc"
+    Link   = "~/src/profile/ruby/pryrc"
   },
   @{
     Source = "~/.vimrc"
-    Link   = "~/src/my/profile/vim/vimrc"
+    Link   = "~/src/profile/vim/vimrc"
   },
   @{
     Source = "$($env:APPDATA)\Code - Insiders\User\settings.json"
-    Link   = "~/src/my/profile/vscode/settings.json"
+    Link   = "~/src/profile/vscode/settings.json"
   },
   @{
     Source = "$($env:APPDATA)\Code - Insiders\User\keybindings.json"
-    Link   = "~/src/my/profile/vscode/keybindings.json"
+    Link   = "~/src/profile/vscode/keybindings.json"
   }
 )
 
