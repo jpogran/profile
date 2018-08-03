e9468f9a4e6bb9b2c0a442519d5e33fb2f8b2895
jpogran <james.pogran@puppet.com>
2018-08-03 14:14:05 -0400

    updates

--- setup.ps1
+++ setup.ps1
@@ -1,51 +1,51 @@
 $config = @(
   @{
     Source = "~/.gitconfig"
-    Link   = "~/src/profile/git/gitconfig"
+    Link   = "~/src/jpogran/profile/git/gitconfig"
   },
   @{
     Source = "~/.gitignore"
-    Link   = "~/src/profile/git/gitignore"
+    Link   = "~/src/jpogran/profile/git/gitignore"
   },
   @{
     Source = "~/Documents/PowerShell/profile.ps1"
-    Link   = "~/src/profile/pwsh/profile.ps1"
+    Link   = "~/src/jpogran/profile/pwsh/profile.ps1"
   },
   @{
     Source = "~/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
-    Link   = "~/src/profile/pwsh/Microsoft.PowerShell_profile.ps1"
+    Link   = "~/src/jpogran/profile/pwsh/Microsoft.PowerShell_profile.ps1"
   },
   @{
     Source = "~/Documents/PowerShell/Microsoft.VSCode_profile.ps1"
-    Link   = "~/src/profile/pwsh/Microsoft.VSCode_profile.ps1"
+    Link   = "~/src/jpogran/profile/pwsh/Microsoft.VSCode_profile.ps1"
   },
   @{
     Source = "~/.gemrc"
-    Link   = "~/src/profile/ruby/gemrc"
+    Link   = "~/src/jpogran/profile/ruby/gemrc"
   },
   @{
     Source = "~/.pryrc"
-    Link   = "~/src/profile/ruby/pryrc"
+    Link   = "~/src/jpogran/profile/ruby/pryrc"
   },
   @{
     Source = "~/.vimrc"
-    Link   = "~/src/profile/vim/vimrc"
+    Link   = "~/src/jpogran/profile/vim/vimrc"
   },
   @{
     Source = "$($env:APPDATA)\Code\User\settings.json"
-    Link   = "~/src/profile/vscode/settings.json"
+    Link   = "~/src/jpogran/profile/vscode/settings.json"
   },
   @{
     Source = "$($env:APPDATA)\Code\User\keybindings.json"
-    Link   = "~/src/profile/vscode/keybindings.json"
+    Link   = "~/src/jpogran/profile/vscode/keybindings.json"
   },
   @{
     Source = "$($env:APPDATA)\Code - Insiders\User\settings.json"
-    Link   = "~/src/profile/vscode/settings.json"
+    Link   = "~/src/jpogran/profile/vscode/settings.json"
   },
   @{
     Source = "$($env:APPDATA)\Code - Insiders\User\keybindings.json"
-    Link   = "~/src/profile/vscode/keybindings.json"
+    Link   = "~/src/jpogran/profile/vscode/keybindings.json"
   }
 )
 
